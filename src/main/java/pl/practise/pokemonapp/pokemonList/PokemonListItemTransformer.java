b2abd718e222352c4fe619d1b43649a476e67de2
Jakub Pietraszewski <jk.pietraszewski@gmial.com>
2021-10-02 13:35:51 +0200

    add pokemon list item endpoint

---
+++ src/main/java/pl/practise/pokemonapp/pokemonList/PokemonListItemTransformer.java
@@ -0,0 +1,15 @@
+package pl.practise.pokemonapp.pokemonList;
+
+import org.springframework.stereotype.Component;
+import pl.practise.pokemonapp.pokemonDetails.PokemonDetails;
+
+@Component
+class PokemonListItemTransformer {
+
+    PokemonListItem toEntity(PokemonDetails pokemonDetails) {
+        PokemonListItem pokemonListItem = new PokemonListItem();
+        pokemonListItem.setName(pokemonDetails.getName());
+        pokemonListItem.setImageUrl(pokemonDetails.getImageUrl());
+        return pokemonListItem;
+    }
+}
